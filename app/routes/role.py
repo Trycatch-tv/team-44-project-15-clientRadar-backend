9e788bbdf834720e0f32a1d0e5b86bac0a812840
imCsCorp <camilo.alejandro.soto.v@gmail.com>
2023-04-21 19:37:05 -0500

    consulta de roles

--- app/routes/role.py
+++ app/routes/role.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Path
+from fastapi.encoders import jsonable_encoder
+from typing import List
 from fastapi.responses import JSONResponse
 
 from app.config.database import Session
@@ -17,13 +19,19 @@ def role_create(role: Role) -> dict:
 
 
 @role.get("/roles", tags=["Role"], description="Get a list of all roles",)
-def role_find():
-    return []
+def role_find() -> List[Role]:
+    db = Session()
+    result = db.query(RoleModel).all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 
-@role.get("role/{id}", tags=["Role"])
-def role_find_by_id(id: int = Path(ge=1)):
-    return "hola"
+@role.get("/role/{id}", tags=["Role"])
+def role_find_by_id(id: int = Path(ge=1)) -> Role:
+    db = Session()
+    result = db.query(RoleModel).filter(RoleModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content={})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 
 @role.put("/role/{id}", tags=["Role"])
