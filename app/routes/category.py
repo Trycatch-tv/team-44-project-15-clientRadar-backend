869ba1f0e891c3f025a14d3d49fcbe1a16b1fd1a
imCsCorp <camilo.alejandro.soto.v@gmail.com>
2023-04-23 22:13:03 -0500

    utilizar la session que se tiene

--- app/routes/category.py
+++ app/routes/category.py
@@ -1,33 +1,45 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException
 from typing import List
 
-from app.schemas.category import CategorySchema # importamos el schema correspondiente
-from app.models.category import CategoryModel # importamos el modelo correspondiente
-from app.database import get_db # importamos la función que nos permitirá obtener una sesión de la base de datos
+# importamos el schema correspondiente
+from app.schemas.category import CategorySchema
+# importamos el modelo correspondiente
+from app.models.category import CategoryModel
+# importamos la función que nos permitirá obtener una sesión de la base de datos
+from app.config.database import Session
 
 router = APIRouter()
 
 # Definimos el endpoint para crear una categoría
+
+
 @router.post("/category", response_model=CategorySchema)
-def create_category(category: CategorySchema, db: Session = Depends(get_db)):
+def create_category(category: CategorySchema):
+    db = Session()
     # Creamos una nueva instancia del modelo CategoryModel y la agregamos a la base de datos
-    db_category = CategoryModel(name=category.name, description=category.description)
+    db_category = CategoryModel(
+        name=category.name, description=category.description)
     db.add(db_category)
     db.commit()
     db.refresh(db_category)
-    return db_category # Retornamos la categoría recién creada
+    return db_category  # Retornamos la categoría recién creada
 
 # Definimos el endpoint para obtener todas las categorías
+
+
 @router.get("/category", response_model=List[CategorySchema])
-def read_categories(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_categories(skip: int = 0, limit: int = 100, ):
+    db = Session()
     # Obtenemos todas las categorías y las retornamos
     categories = db.query(CategoryModel).offset(skip).limit(limit).all()
     return categories
 
 # Definimos el endpoint para actualizar una categoría
+
+
 @router.put("/category/{id}", response_model=CategorySchema)
-def update_category(id: int, category: CategorySchema, db: Session = Depends(get_db)):
+def update_category(id: int, category: CategorySchema, ):
+    db = Session()
     # Buscamos la categoría correspondiente en la base de datos
     db_category = db.query(CategoryModel).filter(CategoryModel.id == id)
     # Si no encontramos la categoría, lanzamos una excepción 404
@@ -37,11 +49,14 @@ def update_category(id: int, category: CategorySchema, db: Session = Depends(get
     db_category.update(category.dict())
     db.commit()
     db_category = db_category.first()
-    return db_category # Retornamos la categoría actualizada
+    return db_category  # Retornamos la categoría actualizada
 
 # Definimos el endpoint para eliminar una categoría
+
+
 @router.delete("/category/{id}")
-def delete_category(id: int, db: Session = Depends(get_db)):
+def delete_category(id: int):
+    db = Session()
     # Buscamos la categoría correspondiente en la base de datos
     db_category = db.query(CategoryModel).filter(CategoryModel.id == id)
     # Si no encontramos la categoría, lanzamos una excepción 404
@@ -50,4 +65,5 @@ def delete_category(id: int, db: Session = Depends(get_db)):
     # Cambiamos el estado de la categoría a "False" (en lugar de eliminarla de la base de datos)
     db_category.update({"state": False})
     db.commit()
-    return {"detail": "Category deleted successfully"} # Retornamos un mensaje de éxito
+    # Retornamos un mensaje de éxito
+    return {"detail": "Category deleted successfully"}
